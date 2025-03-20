6ea1f0c3e64c93a6cd8cf840b13758a0df3f6e6e
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-03-20 16:24:13 -0500

    Inicio16

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <Navbar />
-    <Routes>
+    <Routes base="/WEB-CSD/">
       <Route path="/" element={<Home />} />
       <Route path="/sobre-nosotros" element={<About />} />
       <Route path="/noticias" element={<News />} />
