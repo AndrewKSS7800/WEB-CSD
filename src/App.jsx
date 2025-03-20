646cc440ded190044169932fbc1b339e1a221de0
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-03-20 16:35:25 -0500

    Inicio20

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,18 @@ import About from './pages/about';
 import News from './pages/news';
 import Contact from './pages/contact';
 import Navbar from './components/navbar';
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    // Si el usuario está en la raíz "/WEB-CSD", lo redirige a Inicio
-    if (location.pathname === "/WEB-CSD/") {
-      window.location.replace("/WEB-CSD/inicio");
-    }
-  }, [location]);
-
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="inicio" element={<Home />} />
-        <Route path="sobre-nosotros" element={<About />} />
-        <Route path="noticias" element={<News />} />
-        <Route path="contacto" element={<Contact />} />
-        <Route path="*" element={<Navigate to="inicio" />} /> {/* Redirige cualquier URL inválida a Inicio */}
-      </Routes>
+    <Navbar />
+    <Routes >
+      <Route path="" element={<Home />} />
+      <Route path="sobre-nosotros" element={<About />} />
+      <Route path="noticias" element={<News />} />
+      <Route path="contacto" element={<Contact />} />
+    </Routes>
     </>
   );
 }
