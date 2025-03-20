b093c37c7875b21a1979cef78153b50d8547a21d
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-03-20 16:21:12 -0500

    Inicio13

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/WEB-CSD/">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </StrictMode>,
